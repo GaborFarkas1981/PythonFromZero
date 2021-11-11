7cafde0d4904054d880527d03b8c3d7d51bdeeff
gabor <info@gfarkas.com>
2021-11-11 08:23:53 +0100

    - some math

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+from math import *
+
+my_num = -5.8
+print(abs(my_num))  # builtins
+print(round(abs(pow(my_num, 5))))   # builtins
+print(floor(3.7))   # math
+print(ceil(3.7))   # math
+print(sqrt(36))   # math
