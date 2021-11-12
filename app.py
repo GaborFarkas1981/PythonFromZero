0e738631834998c53177092da7fc122e2e2fc80f
gabor <info@gfarkas.com>
2021-11-12 09:44:00 +0100

    - Sklearn

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-friends = ["Kevin", "Karen", "Jim"]
-print(friends[-1])
+import matplotlib.pyplot as plt
+
+x = [i for i in range(10)]
+print(x)
+y = [2 * i for i in range(10)]
+print(y)
+
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.plot(x, y)
+plt.show()
